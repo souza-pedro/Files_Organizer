0711f56af2a4dfc0637861db342f9631c5939d33
Pedro <ordepo970>
2019-11-05 21:11:57 -0300

    inicio de uso do pandas para limpeza dos dados

--- PYTHON/Import_Conf.py
+++ PYTHON/Import_Conf.py
@@ -11,6 +11,11 @@ import sys
 conf = pd.read_csv("PYTHON/PEP0000903846_Conf2019 - Copy.TXT", sep = "|", skiprows = 3, encoding = "ISO-8859-1", usecols=lambda x: x not in ['Unnamed: 0','Unnamed: 25'])
 test0 = pd.read_csv("PYTHON/PEP0000903846_Conf2019 - Copy.TXT", sep = "|", skiprows = 3, encoding = "ISO-8859-1")
 
+conf2 = conf[conf['  Ordem     ']!='  Ordem     ']
+conf_obj = Conf.select_dtypes(['object'])
+Conf[conf_obj.columns] = conf_obj.apply(lambda x: x.str.strip())
+
+conf2 = conf[conf['  Ordem     ']!='Ordem']
 '''print(open('PYTHON/PEP0000903846_Conf2019 - Copy.TXT').read())'''
 
 main()
